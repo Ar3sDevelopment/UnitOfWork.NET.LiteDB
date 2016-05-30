bd6bd038404ae1302571ce058e373bf3c556b3f8
Matteo Bruni <matteo.bruni@me.com>
2016-05-30 12:22:19 +0200

    fix assembly info

--- UnitOfWork.NET.LiteDB/Properties/AssemblyInfo.cs
+++ UnitOfWork.NET.LiteDB/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UnitOfWork.NET.LiteDB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("UnitOfWork for LiteDB")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Caelan")]
 [assembly: AssemblyProduct("UnitOfWork.NET.LiteDB")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
